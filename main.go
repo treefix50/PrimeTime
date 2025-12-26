ef954d260f8f448cb16f0b48807059111f593bf3
treefix50 <merisd@outlook.de>
2025-12-26 22:17:27 +0100

    Initialize main.go with server setup and signal handling

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/DEINUSER/primetime/internal/server"
+)
+
+func main() {
+	var (
+		root = flag.String("root", "./media", "media root directory")
+		addr = flag.String("addr", ":8080", "listen address")
+	)
+	flag.Parse()
+
+	s, err := server.New(*root, *addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// graceful-ish stop
+	go func() {
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+		<-ch
+		log.Println("shutting down...")
+		_ = s.Close()
+	}()
+
+	log.Printf("PrimeTime listening on http://localhost%s (root=%s)\n", *addr, *root)
+	log.Fatal(s.Start())
+}
