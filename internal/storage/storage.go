d8478ccb83ba6ea051560d2815bb659fbe54f33c
treefix50 <merisd@outlook.de>
2025-12-26 23:28:52 +0100

    Add sqlite storage package

---
+++ internal/storage/storage.go
@@ -0,0 +1,37 @@
+package storage
+
+import (
+	"database/sql"
+
+	_ "modernc.org/sqlite"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func Open(path string) (*Store, error) {
+	db, err := sql.Open("sqlite", path)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := db.Exec("PRAGMA foreign_keys=ON"); err != nil {
+		_ = db.Close()
+		return nil, err
+	}
+
+	if _, err := db.Exec("PRAGMA journal_mode=WAL"); err != nil {
+		_ = db.Close()
+		return nil, err
+	}
+
+	return &Store{db: db}, nil
+}
+
+func (s *Store) Close() error {
+	if s == nil || s.db == nil {
+		return nil
+	}
+	return s.db.Close()
+}
--- main.go
+++ main.go
@@ -6,16 +6,19 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
 
 	"github.com/treefix50/primetime/internal/ffmpeg"
 	"github.com/treefix50/primetime/internal/server"
+	"github.com/treefix50/primetime/internal/storage"
 )
 
 func main() {
 	var (
 		root = flag.String("root", "./media", "media root directory")
 		addr = flag.String("addr", ":8080", "listen address")
+		db   = flag.String("db", defaultDBPath(), "sqlite database path (env PRIMETIME_DB)")
 	)
 	flag.Parse()
 
@@ -30,6 +33,15 @@ func main() {
 	}
 	log.Printf("ffmpeg: %s\n", ff)
 
+	if err := ensureDBDir(*db); err != nil {
+		log.Fatal(err)
+	}
+
+	store, err := storage.Open(*db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	s, err := server.New(*root, *addr)
 	if err != nil {
 		log.Fatal(err)
@@ -40,9 +52,24 @@ func main() {
 		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 		<-ch
 		log.Println("shutting down...")
+		_ = store.Close()
 		_ = s.Close()
 	}()
 
 	log.Printf("PrimeTime listening on http://localhost%s (root=%s)\n", *addr, *root)
 	log.Fatal(s.Start())
+}
+
+func defaultDBPath() string {
+	if value := os.Getenv("PRIMETIME_DB"); value != "" {
+		return value
+	}
+	return "data/primetime.db"
+}
+
+func ensureDBDir(path string) error {
+	if path == ":memory:" {
+		return nil
+	}
+	return os.MkdirAll(filepath.Dir(path), 0o755)
 }
